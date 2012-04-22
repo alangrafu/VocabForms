fa86d61c822ecdc454ebf7a57054091d313edb03
alvaro <alvaro@alia.(none)>
2012-04-21 23:53:07 -0700

    Use of span instead of inputs in read-only
    add links to object data

--- static/js/view.js
+++ static/js/view.js
@@ -7,30 +7,29 @@ $(function(){
         button = "#add_"+x[1];
         prev = "#field_"+(x[0]-1)+separator+x[1];
         clss = $(prev).attr("class");
-        $(button).before('<br/><input disabled="true" size="100" type="text" id="field_'+i+'" class="'+clss+'" value="" />');
+        $(button).before('<br/><input size="100" type="text" id="field_'+i+'" class="'+clss+'" value="" />');
 $("input:not(.rdfs\\:Literal)").each(function(i, item){
         autocomplete(item);
     });        console.log("#field_"+i);
+        $("#data").append('<input size="100" type="hidden" id="'+i+'" value=""/>');
       }      
       if(visible == "rdf\\:type"){
-        $("#1"+separator+"a").val(values[i].value);
+        $("#1"+separator+"a").html(values[i].value);
       }else if(visible == "1"+separator+"rdfs\\:label"){
-        $("#1"+separator+"rdfs\\:label").val("\""+values[i].value+"\"");
-        $("#field_1"+separator+"rdfs\\:label").val(values[i].value);
+        $("#1"+separator+"rdfs\\:label").html("\""+values[i].value+"\"");
+        $("#field_1"+separator+"rdfs\\:label").html(values[i].value);
       }else if(visible == "1"+separator+"rdfs\\:comment"){
-        $("#1"+separator+"rdfs\\:comment").val("\""+values[i].value+"\"");
-        $("#field_1"+separator+"rdfs\\:comment").val(values[i].value);
+        $("#1"+separator+"rdfs\\:comment").html("\""+values[i].value+"\"");
+        $("#field_1"+separator+"rdfs\\:comment").html(values[i].value);
       }else{        
         if(values[i].is_object){
+          myLabel = values[i].value;
           if(values[i].label.length > 0){
-          $("#field_"+visible).val(values[i].label);
-          }else{
-          $("#field_"+visible).val(values[i].value);         
+            myLabel = values[i].label;
           }
-          $("#"+visible).val("<"+values[i].value+">");
+          $("#field_"+visible).html("<a href='"+values[i].value+"'>"+myLabel+"</a>");
         }else{
-          $("#field_"+visible).val(values[i].value);
-          $("#"+visible).val("\""+values[i].value+"\"");
+          $("#field_"+visible).html(values[i].value);
         }
       }
     };
